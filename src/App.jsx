03520321b78d758235a29b6fe92f76b4c6da47f2
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2026-01-28 23:42:30 +0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
 import HomeCOC from "./pages/HomeCOC";
-import Tess from "./pages/Tess";
-import Page2 from "./pages/page2";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="admin" element={<Dashboard />} />
-
-        <Route path="/coc" element={<HomeCOC />} />
-        <Route path="/tes" element={<Tess />} />
-        <Route path="/clan" element={<Page2 />} />
+        <Route path="/" element={<HomeCOC />} />
       </Routes>
     </BrowserRouter>
   );
